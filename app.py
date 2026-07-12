41dea35d7665a1f066aaee1227c42dd58b03bf3d
Akash sharma <akash6392614083@gmail.com>
2026-07-12 02:15:21 -0700

    seperate file maked for registration by akash

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@app.route("/candidate")
+def candidate():
+    return render_template("candidate.html")
+
+@app.route("/recruiter")
+def recruiter():
+    return render_template("recruiter.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
